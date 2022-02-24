c602cf4dcf1b331da7a3882c4cb9d0d2a9dc234b
Alessandro Giacche <alejack9@users.noreply.github.com>
2022-02-24 20:41:05 +0100

    tmp

--- optimizeSimplified.js
+++ optimizeSimplified.js
@@ -1,4 +1,16 @@
+/**
+ *
+ * @param {*} devs
+ * @param {[{name: str, bestBefore: int, duration: int, fullScore: int, actualScore: int, reqSkills: [{AAAA}]}]} projects
+ * @returns
+ */
 export default function optimizeSimplified(devs, projects) {
-  let projectsCompleted = []
-  return projectsCompleted
+  let projectsCompleted = [];
+
+  const orderedProject = projects.sort(
+    (pr1, pr2) => pr1.duration - pr2.duration
+  );
+  console.log(orderedProject);
+
+  return projectsCompleted;
 }
