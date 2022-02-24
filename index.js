32502ae078eac0ab4f454f9dff7245ecd1fc4ef2
Elena Padovani <elena.padovani@mia-care.io>
2022-02-24 20:23:08 +0100

    function simplified

--- index.js
+++ index.js
@@ -2,19 +2,18 @@
 // const fs = require("fs");
 import get from "./input.js";
 import write from "./output.js"
+import optimizeSimplified from "./optimizeSimplified"
 
 const index = function () {
   const fileName = process.argv.pop();
   const { devs, projects } = get(fileName);
 
-  const projectsCompleted = [
+/*   const projectsCompleted = [
     { name:'logging', roles: ['Anna', 'Bob'] },
     { name:'server', roles: ['Anna'] }
-  ]
+  ] */
+  const projectsCompleted = optimizeSimplified(devs, projects)
   write(projectsCompleted, 'pippo.txt')
-  //   console.log({ devs, projects }); // OK
-  //   devs.forEach((d) => console.log(d.skills)); // OK
-  //   projects.forEach((d) => console.log(d.reqSkills)); // OK
 };
 
 index();
---
+++ optimizeSimplified.js
@@ -0,0 +1,4 @@
+export default function optimizeSimplified(devs, projects) {
+  let projectsCompleted = []
+  return projectsCompleted
+}
