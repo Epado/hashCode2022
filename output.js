6d2dfeac573cad9cc2d78a707b6e737cb4b6a143
Elena Padovani <elena.padovani@mia-care.io>
2022-02-24 20:15:21 +0100

    fixed output

--- output.js
+++ output.js
@@ -1,25 +1,12 @@
 import * as fs from "fs";
 import {range} from './utils.js';
 
-/* projectsCompleted
-- name 
-- list of names 
- */
-const stringify = function (s){
-  let resString = ''
-  for(let i = 0; i < s.length; i++){
-      let elem = s[i]
-      elem = typeof elem === 'string' ? elem : elem.join(" ")
-      resString += (elem + '\n')
-  }
-  return resString
-}
-
 export default function writeResult(projectsCompleted, outfileName) {
 
   const porjectsNum = projectsCompleted.length.toString()
   let resString = porjectsNum + '\n'
   for(let idx=0; idx < projectsCompleted.length; idx++) {
+    const projectCompleted = projectsCompleted[idx]
     resString += projectCompleted.name
     resString += '\n'
 
